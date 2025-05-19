63ff3230ac1ef080b0943e17238691a99d410e93
Hedi Zisling <43644953+Zisling@users.noreply.github.com>
2025-05-19 19:41:46 +0300

    solid run fixes

--- configs/config_router.py
+++ configs/config_router.py
@@ -6,34 +6,35 @@ def get_configs():
     Load the configs, override default configs with argument parsed configs
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_name', type=str, required=True)
+    parser.add_argument('--config_name', '--config', type=str, required=True)
 
     # --- Koopman configs --- #
-    if 'koopman/cifar_uncond' == parser.parse_known_args()[0].config_name:
+    config_name = parser.parse_known_args()[0].config_name
+    if 'koopman/cifar_uncond' == config_name:
         from configs.koopman.cifar_uncond import load_arguments
         load_arguments(parser)
 
-    elif 'koopman/cifar_uncond_decomposed' == parser.parse_known_args()[0].config_name:
+    elif 'koopman/cifar_uncond_decomposed' == config_name:
         from configs.koopman.cifar_uncond_decomposed import load_arguments
         load_arguments(parser)
 
-    elif 'koopman/cifar_cond' == parser.parse_known_args()[0].config_name:
+    elif 'koopman/cifar_cond' == config_name:
         from configs.koopman.cifar_cond import load_arguments
         load_arguments(parser)
 
-    elif 'koopman/ffhq_uncond' == parser.parse_known_args()[0].config_name:
+    elif 'koopman/ffhq_uncond' == config_name:
         from configs.koopman.ffhq_uncond import load_arguments
         load_arguments(parser)
 
-    elif 'koopman/afhq_uncond' == parser.parse_known_args()[0].config_name:
+    elif 'koopman/afhq_uncond' == config_name:
         from configs.koopman.afhq_uncond import load_arguments
         load_arguments(parser)
 
-    elif 'koopman/checkerboard_uncond' == parser.parse_known_args()[0].config_name:
+    elif 'koopman/checkerboard_uncond' == config_name:
         from configs.koopman.checkerboard_uncond import load_arguments
         load_arguments(parser)
     else:
-        raise ModuleNotFoundError(f"No such config file {parser.parse_known_args()[0].config_name}")
+        raise ModuleNotFoundError(f"No such config file {config_name}")
 
     args = parser.parse_args()
     print(args)
